fc36494677d33e96b62eaf66259d7d9232c7a106
Lmraza98 <lmraza98@gmail.com>
2023-02-13 07:37:37 -0500

    updated middleware.js for s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,39 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    // ~/strapi-aws-s3/backend/config/middlewares.js
+    
+    module.exports = ({ env }) = [
+      'strapi::errors',
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                env('AWS_BUCKET')+'.s3.'+env('AWS_REGION')+'.amazonaws.com'
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                env('AWS_BUCKET')+'.s3.'+env('AWS_REGION')+'.amazonaws.com'
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      },
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
